6e1b26803a8f57b00d872a4b6ad3f3a1cc287e26
tejashri42@gmail.com <tejashri42@gmail.com>
2020-11-20 03:01:22 +0530

    done still functional componet

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Vehicle from './Vehicle/vehicle';
 function App() {
   return (
     <div className="App">
+      <h1>List Of Vehicles</h1>
       <Vehicle VehicleType="Car" Name="Altroz" Manufacture="Tata" Fuel="Petrol"/>
       <Vehicle VehicleType="Car" Name="BMW" Manufacture="BMW" Fuel="Disel"/>
       <Vehicle VehicleType="Car" Name="Nano" Manufacture="Tata" Fuel="Petrol"/>
